def6d049063f46b3484608abf416505db8c3b152
David Mann <davidmann10k@gmail.com>
2019-02-28 00:04:26 -0600

    cutting the cruft

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::SessionsController < Devise::SessionsController
+  # no authenticity token in API
   skip_before_action :verify_authenticity_token
-  prepend_before_action :require_no_authentication, only: [:create]
-  skip_before_action :verify_signed_out_user, only: [:destroy]
-  respond_to :json
-
-  def destroy
-    authenticate_user!
-    puts "LOG OUT"
-    super
-  end
 
   private
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,10 @@ class PagesController < ApplicationController
   end
 
   def secret
+    authenticate_user!
     respond_to do |format|
-      format.html {
-        authenticate_user!
-      }
+      format.html
       format.json {
-        authenticate_user!
         render json: { response: "This is the secret sauce!" }, status: 200
       }
     end
